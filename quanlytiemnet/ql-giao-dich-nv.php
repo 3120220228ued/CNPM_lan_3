60f24fa22f05c6dbde1b2ffd9816f8be2a83b79b
3120220228ued <117795439+3120220228ued@users.noreply.github.com>
2022-12-27 15:30:36 +0700

    Update ql-giao-dich-nv.php

--- quanlytiemnet/ql-giao-dich-nv.php
+++ quanlytiemnet/ql-giao-dich-nv.php
@@ -6,6 +6,18 @@ require_once('./db_connect.php');
 require_once('./include/function.php');
 require_once('./header-nv.php');
 
+$querykhachhang = mysqli_query($conn, "SELECT * FROM `thongtinkhachhang`");
+$querygia = mysqli_query($conn, "SELECT * FROM `giatien` WHERE 1 ORDER BY `idgiatien` DESC");
+$querymay = mysqli_query($conn, "SELECT `tenmay` FROM `maytinh` WHERE `id`=" . $_GET['may']);
+$maytinh = mysqli_fetch_assoc($querymay);
+if (isset($_POST['submit'])) {
+    $query = mysqli_query($conn, "INSERT INTO `giaodich` (`sdtkhachhang`, `idmay`, `idgiatien`, `thoigianbatdau`, `giamgia`, `ghichu`) VALUES ('" . $_POST['khachhang'] . "', '" . $_POST['idmay'] . "', '" . $_POST['giatien'] . "', '" . $_POST['thoigianbatdau'] . "', '" . $_POST['giamgia'] . "', '" . $_POST['ghichu'] . "');");
+    if ($query) {
+        echo "<script>Swal.fire('Thành công!','Đã lưu thông tin giao dịch!','success').then(function(){window.location = './index-nv.php';});</script>";
+    } else {
+        echo "<script>Swal.fire('Thất bại!','Không thể lưu thông tin giao dịch!','error');</script>";
+    }
+}
 
 ?>
 
