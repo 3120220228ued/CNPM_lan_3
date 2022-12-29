a07229ef63493d5264df5c947884c8080bce5321
3120220228ued <117795439+3120220228ued@users.noreply.github.com>
2022-12-29 14:24:08 +0700

    Update header.php

--- quanlytiemnet/header.php
+++ quanlytiemnet/header.php
@@ -31,8 +31,17 @@ if (!isset($_SESSION['taikhoan']) || $_SESSION['taikhoan'] == "")
   <script src="//cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
   
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="//cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
   <!-- select2 -->
-  
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+	<script src="https://hotrolaptrinh.github.io/js/tech/tech.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>  
+  <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+  <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
   
 
 </head>
